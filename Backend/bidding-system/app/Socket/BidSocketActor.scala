d9345930f93894d904266a54d45d320c566482c1
r kip <gjr.kip@gmail.com>
2019-10-27 12:42:30 +0100

    working share but no update of other sockets

--- Backend/bidding-system/app/Socket/BidSocketActor.scala
+++ Backend/bidding-system/app/Socket/BidSocketActor.scala
@@ -4,13 +4,15 @@ import akka.actor._
 import database.DatabaseUtils
 import play.api.libs.json.Json
 
+import scala.collection.mutable
+
 object BidSocketActor {
   def props(out: ActorRef) = Props(new BidSocketActor(out))
+  var bids: mutable.Map[String, Seq[Int]] = mutable.Map()
 }
 
 class BidSocketActor(out: ActorRef) extends Actor {
 
-  var bids: Seq[Int] = Seq()
   var product_id = "0"
 
   def receive: PartialFunction[Any, Unit] = {
@@ -18,18 +20,20 @@ class BidSocketActor(out: ActorRef) extends Actor {
       val productJson = Json.parse(json)
       val product_id = productJson("productId").as[String]
       val price = productJson("price").as[String]
-      if(product_id != this.product_id){
-        this.product_id = product_id
-        this.bids = Seq(1, 2, 5)//TODO: init from db
+      if(this.product_id != product_id) {
+          this.product_id = product_id
+      }
+      if(BidSocketActor.bids.get(product_id).isEmpty) {
+        BidSocketActor.bids(product_id) = Seq(1, 2, 5) //TODO: init from db
       }
       this.bidPrice(product_id, price.toInt)
-      val send = Json.stringify(DatabaseUtils.convertBidsToJson(this.bids))
+      val send = Json.stringify(DatabaseUtils.convertBidsToJson(BidSocketActor.bids(this.product_id)))
       println("Sending: " + send) //TODO: debug/log remove later
       out ! (send)
   }
 
   def bidPrice(product_id : String, price : Int) = {
-    this.bids = this.bids :+ price
+    BidSocketActor.bids(product_id) = BidSocketActor.bids(product_id) :+ price
     //TODO: insert bid in db
     //TODO: insert bid in mq
   }
